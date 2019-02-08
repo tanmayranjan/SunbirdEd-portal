35916f94c99bf25c382357fa70b6c6836626c8f5
sumitnautiyal-sunbird <47247007+sumitnautiyal-sunbird@users.noreply.github.com>
2019-02-08 13:36:02 +0530

    crate confExtract file
    to extract the information to build the docker image

---
+++ confExtract.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Build script
+# set -o errexit
+e () {
+    echo $( echo ${1} | jq ".${2}" | sed 's/\"//g')
+}
+m=$(./src/app/metadata.sh)
+
+org=$(e "${m}" "org")
+hubuser=$(e "${m}" "hubuser")
+name=$(e "${m}" "name")
+version=$(e "${m}" "version")
+
+artifactLabel=${ARTIFACT_LABEL:-bronze}
