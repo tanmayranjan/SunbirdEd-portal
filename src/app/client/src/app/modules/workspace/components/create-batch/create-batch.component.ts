4fab1c9c37f15387e93bc04f0ef841f151f550fd
Aggidevara <swetha.aggi123@gmail.com>
2019-03-12 19:02:56 +0530

    errors resloved

--- src/app/client/src/app/modules/workspace/components/create-batch/create-batch.component.ts
+++ src/app/client/src/app/modules/workspace/components/create-batch/create-batch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create-batch',
@@ -9,8 +10,11 @@ import { Router } from '@angular/router';
 export class CreateBatchComponent implements OnInit {
   @ViewChild('modal') modal;
   today = new Date();
-  endDate = new Date(this.today.getTime() + (24 * 60 * 60 * 1000));
+  startDate = new FormControl(new Date().toISOString());
+  endDate = new FormControl();
 @Input() courseId;
+name = new FormControl();
+description = new FormControl();
   constructor(
     public router: Router
   ) { }
@@ -19,7 +23,18 @@ export class CreateBatchComponent implements OnInit {
     console.log(this.courseId);
   }
 createBatch() {
-
+  // const requestBody = {
+  //   courseId: this.courseId,
+  //   name: this.batchnameCtrl.value,
+  //   description: this.batchDescriptCtrl.value,
+  //   // tslint:disable-next-line:quotemark
+  //   enrollmentType: 'invite-only',
+  //   startDate: startDate,
+  //   endDate: endDate || null,
+  //   createdBy: this.userService.userid,
+  //   createdFor: this.userService.userProfile.organisationIds,
+  //   mentors: _.compact(mentorIds)
+  // };
 }
 goToCreate() {
   this.router.navigate(['/workspace/content/allcontent', 1]);
