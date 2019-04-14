ed61cad02b48363d93ed775687bb346dbe9dd292
Rishabh Kalra <Rishabh.Kalra@niit.com>
2019-04-14 21:24:00 +0530

    added footer UI

--- src/app/client/src/app/modules/core/components/main-footer/main-footer.component.ts
+++ src/app/client/src/app/modules/core/components/main-footer/main-footer.component.ts
@@ -5,7 +5,7 @@ import * as _ from 'lodash';
 @Component({
   selector: 'app-footer',
   templateUrl: './main-footer.component.html',
-  styleUrls: ['./main-footer.component.css']
+  styleUrls: ['./main-footer.component.scss']
 })
 export class MainFooterComponent implements OnInit {
   /**
